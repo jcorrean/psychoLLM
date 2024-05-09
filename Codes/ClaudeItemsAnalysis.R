4fd652f2ef6ac2de168bcd29d925d9bef1d693de
Juan C. Correa <j.correa.n@gmail.com>
2024-05-08 23:25:23 -0600

    Update ClaudeItemsAnalysis.R

--- Codes/ClaudeItemsAnalysis.R
+++ Codes/ClaudeItemsAnalysis.R
@@ -1,8 +1,8 @@
 library(readr)
-ClaudeTest <- readtext("Data/ClaudeTest.csv")
+ClaudeTest <- read_csv("Data/ClaudeTest.csv")
 ClaudeTest$doc_id <- 1:nrow(ClaudeTest)
 library(quanteda)
-ClaudeItems <- corpus(ClaudeTest$text)
+ClaudeItems <- corpus(ClaudeTest$item)
 docvars(ClaudeItems, "Dimension") <- ClaudeTest$dimension
 docvars(ClaudeItems, "Generating.Source") <- ClaudeTest$item.source
 summary(ClaudeItems)
@@ -15,3 +15,16 @@ Items <- tokens(ClaudeItems,
                    remove_url = TRUE, 
                    remove_symbols = TRUE) %>%  
   tokens_remove(stopwords("english"))
+
+existing.items <- corpus_subset(ClaudeItems, Generating.Source == "existing.scale")
+ExistingItems <- dfm(tokens(existing.items), tolower = TRUE)
+tmod <- textmodel_ca(ExistingItems)
+tmod$rowcoord
+
+LLM.items <- corpus_subset(ClaudeItems, Generating.Source == "LLM.generated")
+LLMITEMS <- dfm(tokens(LLM.items), tolower = TRUE)
+
+# Semantic Analysis ----
+library("quanteda.textmodels")
+SA.ExistingItems <- textmodel_lsa(ExistingItems)
+SA.ExistingItems$features
